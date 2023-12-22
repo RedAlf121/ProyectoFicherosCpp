3b957cffe60aa48ef7c4c7abea10aabfe2493d7b
javi484 <148660179+javi484@users.noreply.github.com>
2023-12-21 23:18:58 -0500

    Validación en la visual Insertar
    Restricción en los lineText

--- Proyecto_Optativa/gui/UfrmDialogInsert/UfrmDialogInsert.cpp
+++ Proyecto_Optativa/gui/UfrmDialogInsert/UfrmDialogInsert.cpp
@@ -1,9 +1,14 @@
 #include "UfrmDialogInsert.h"
+#include <QRegularExpressionValidator>
+
 
 InsertDialog::InsertDialog(QWidget *parent) :
     QDialog(parent)
 {
     ui.setupUi(this);
+    ui.lineID->setValidator(new QRegularExpressionValidator(QRegularExpression("[0-9]{0,11}"), this));
+    ui.lineName->setValidator(new QRegularExpressionValidator(QRegularExpression("[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]*"), this));
+    ui.lineLastNames->setValidator(new QRegularExpressionValidator(QRegularExpression("[a-zA-ZáéíóúÁÉÍÓÚüÜñÑ ]*"), this));
     this->setWindowTitle("Insertar estudiante");
     this->setFixedSize(QSize(618, 376));
 }
